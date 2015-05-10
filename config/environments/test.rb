f7b3db1738a07ab04e98e627655db1a0f32297b4
yanaulianova <yanaulianova@gmail.com>
2015-05-10 17:59:04 +0300

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
-  get 'users/new'
+SampleApp::Application.routes.draw do
+  
+  resources :users
 
   root 'static_pages#home'
 
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
 
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
